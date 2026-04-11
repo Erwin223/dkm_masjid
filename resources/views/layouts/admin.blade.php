9dc0ea34ac29a6ece6854be2552f1696ba3b1235
Agus Cakra Dinata <erwinirving123@gmail.com>
2026-04-11 23:15:06 +0700

    penambahan favicon dan penambahan modul arsip

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,7 @@
     <title>{{ trim($__env->yieldContent('title')) ?: trim($__env->yieldContent('page-title', 'Dashboard Admin')) . ' - DKM' }}</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpeg" href="{{ asset('favicon.ico') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     @stack('styles')
 
